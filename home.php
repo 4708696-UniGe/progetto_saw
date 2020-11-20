cee8a0352d7053f35d7f53c2ee8a407ce1bfaffa
4708696-UniGe <S4708696@studenti.unige.it>
2020-11-20 10:56:43 +0100

    initial home page structure

--- home.php
+++ home.php
@@ -1,2 +1,38 @@
 <!DOCTYPE html>
 <html>
+	<head> 
+	<title> Coming soon </title>
+	<script src="https://kit.fontawesome.com/c1e79cc6ba.js" crossorigin="anonymous"></script>
+	<link rel="stylesheet" type = "text/css" href="home_style.css"/>
+	</head>
+
+	<body>
+		<nav> <?php include 'navbar.php'; ?> </nav>
+
+		<div class="container_right" >
+		<img src="img_avatar.png" alt="Avatar" class="image" >
+		<div class="overlay" >
+		<div class="text" > Hello World</div >
+		</div >
+		</div >
+
+		<div class="container_left" >
+		<img src="img_avatar.png" alt="Avatar" class="image" >
+		<div class="overlay" >
+		<div class="text" > Hello World</div >
+		</div >
+		</div >
+
+		<div class="container_right" >
+		<img src="img_avatar.png" alt="Avatar" class="image" >
+		<div class="overlay" >
+		<div class="text" > Hello World</div >
+		</div >
+		</div >
+
+		<footer> <?php include 'footer.php'; ?> </footer>
+	</body>
+
+
+
+</html>
---
+++ navbar.php
@@ -0,0 +1,7 @@
+<?php  ?>
+ <ul id="nav">
+  <li id="about"><a href="about.php">About</a> </li>
+  <li id="signup"><a href="registration.php">Sign-Up</a> </li>
+  <li id="login"><a href="login.php">Login</a> </li>
+  </a> </li>
+ </ul>
