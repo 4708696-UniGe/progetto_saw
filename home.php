2d7433bdf50154e1ca6c281f8d0ccb41295902e8
Riccardo Guerrisi <S4639439@studenti.unige.it>
2020-11-20 12:39:40 +0100

    aggiunto lo slide text box solo per le immagini sulla sinistra nel master-richi

---
+++ home.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+	<head> 
+	<title> Coming soon </title>
+	<script src="https://kit.fontawesome.com/c1e79cc6ba.js" crossorigin="anonymous"></script>
+	<link rel="stylesheet" type = "text/css" href="home_style.css"/>
+	</head>
+
+	<body>
+		<nav> <?php include 'navbar.php'; ?> </nav>
+
+		<div class="container" >
+		<img src="https://www.zeusnews.it/img/4/8/1/6/2/0/026184-620-google-vedi-immagini.jpg" alt="Avatar" class="image" >
+		<div class="overlay_left" >
+		<div class="text" > Hello World</div >
+		</div >
+		</div >
+
+		<div class="container" >
+		<img style="text-align: center" src="https://www.zeusnews.it/img/4/8/1/6/2/0/026184-620-google-vedi-immagini.jpg" alt="Avatar" class="image" >
+		<div class="overlay_right" >
+		<div class="text" > Hello World</div >
+		</div >
+		</div >
+
+		<div class="container" >
+		<img src="https://www.zeusnews.it/img/4/8/1/6/2/0/026184-620-google-vedi-immagini.jpg" alt="Avatar" class="image" >
+		<div class="overlay_left" >
+		<div class="text" > Hello World</div >
+		</div >
+		</div >
+
+		<footer> <?php include 'footer.php'; ?> </footer>
+	</body>
+
+
+
+</html>
