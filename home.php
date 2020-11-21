6604cb58d916c98d3b5238efbefaaf81377bd506
Riccardo Guerrisi <S4639439@studenti.unige.it>
2020-11-21 20:38:06 +0100

    testing robe

--- home.php
+++ home.php
@@ -2,33 +2,18 @@
 <html>
 	<head> 
 	<title> Coming soon </title>
-	<script src="https://kit.fontawesome.com/c1e79cc6ba.js" crossorigin="anonymous"></script>
 	<link rel="stylesheet" type = "text/css" href="home_style.css"/>
 	</head>
 
 	<body>
 		<nav> <?php include 'navbar.php'; ?> </nav>
 
-		<div class="container" >
-		<img src="https://www.zeusnews.it/img/4/8/1/6/2/0/026184-620-google-vedi-immagini.jpg" alt="Avatar" class="image" >
-		<div class="overlay_left" >
-		<div class="text" > Hello World</div >
-		</div >
-		</div >
-
-		<div class="container" >
-		<img style="text-align: center" src="https://www.zeusnews.it/img/4/8/1/6/2/0/026184-620-google-vedi-immagini.jpg" alt="Avatar" class="image" >
-		<div class="overlay_right" >
-		<div class="text" > Hello World</div >
-		</div >
-		</div >
-
-		<div class="container" >
-		<img src="https://www.zeusnews.it/img/4/8/1/6/2/0/026184-620-google-vedi-immagini.jpg" alt="Avatar" class="image" >
-		<div class="overlay_left" >
-		<div class="text" > Hello World</div >
-		</div >
-		</div >
+		<div class="container">
+			<img src="https://images-na.ssl-images-amazon.com/images/I/51PqLGdZpaL._AC_SX425_.jpg" alt="Avatar" class="image" >
+			<div class="overlay">
+			<div class="text">Hello World</div>
+		</div>
+	</div>
 
 		<footer> <?php include 'footer.php'; ?> </footer>
 	</body>
