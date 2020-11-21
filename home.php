0f76d3952563c4c818799ed78494fb83b76a6b7f
Riccardo Guerrisi <S4639439@studenti.unige.it>
2020-11-21 21:16:58 +0100

    aggiunte immagini overalyed

--- home.php
+++ home.php
@@ -2,22 +2,33 @@
 <html>
 	<head> 
 	<title> Coming soon </title>
-	<link rel="stylesheet" type = "text/css" href="home_style.css"/>
+	<link rel="stylesheet" href="home_style.css">
 	</head>
 
 	<body>
 		<nav> <?php include 'navbar.php'; ?> </nav>
 
-		<div class="container">
+		<div class="container_left">
 			<img src="https://images-na.ssl-images-amazon.com/images/I/51PqLGdZpaL._AC_SX425_.jpg" alt="Avatar" class="image" >
-			<div class="overlay">
+			<div class="overlay_left">
+			<div class="text">La nostra azienda si basa sull'assistenza professionale di aziende, le quali ci possono contattare attraverso il forum.</div>
+		</div>
+	</div>
+
+		<div class="container_right">
+			<img src="https://images-na.ssl-images-amazon.com/images/I/51PqLGdZpaL._AC_SX425_.jpg" alt="Avatar" class="image" >
+			<div class="overlay_right">
+			<div class="text">Hello World</div>
+		</div>
+	</div>
+
+		<div class="container_left">
+			<img src="https://images-na.ssl-images-amazon.com/images/I/51PqLGdZpaL._AC_SX425_.jpg" alt="Avatar" class="image" >
+			<div class="overlay_left">
 			<div class="text">Hello World</div>
 		</div>
 	</div>
 
 		<footer> <?php include 'footer.php'; ?> </footer>
 	</body>
-
-
-
 </html>
