c7081c0a2f7f1acfc4e5de2e2e0fdf7cc9e02dad
Riccardo Guerrisi <S4639439@studenti.unige.it>
2020-11-28 08:48:38 +0100

    modifiche al css (no border)

--- home.php
+++ home.php
@@ -6,29 +6,28 @@
 	</head>
 
 	<body>
-		<nav> <?php include 'navbar.php'; ?> </nav>
+			<nav> <?php include 'navbar.php'; ?> </nav>
 
 		<div class="container_left">
-			<img src="https://www.infoaut.org/media/k2/items/cache/006c7e95702c8679fc1d4c3cdf4084a0_XL.jpg" alt="Avatar" class="image" >
+			<img src="https://www.infoaut.org/media/k2/items/cache/006c7e95702c8679fc1d4c3cdf4084a0_XL.jpg" alt="Avatar" class = "image" >
 			<div class="overlay_left">
-			<div class="text">La nostra azienda si basa sull'assistenza professionale di aziende, le quali ci possono contattare attraverso il forum.</div>
+			<div class="text">La nostra azienda si basa sull'assistenza professionale di aziende,<br> le quali ci possono contattare attraverso il forum.</div>
 		</div>
 	</div>
 
-		<div class="container_right">
+	<div class="container_right">
 			<img src="https://www.tdblog.it/wp-content/uploads/2018/11/market_server.jpg" alt="Avatar" class="image" >
 			<div class="overlay_right">
 			<div class="text">Hello World</div>
 		</div>
 	</div>
 
-		<div class="container_left">
+	<div class="container_left">
 			<img src="https://images-na.ssl-images-amazon.com/images/I/51PqLGdZpaL._AC_SX425_.jpg" alt="Avatar" class="image" >
 			<div class="overlay_left">
-			<div class="text">Hello World</div>
+			<div class="text">La nostra azienda si basa sull'assistenza professionale di aziende,<br> le quali ci possono contattare attraverso il forum.</div>
 		</div>
 	</div>
-
 		<footer> <?php include 'footer.php'; ?> </footer>
 	</body>
 </html>
