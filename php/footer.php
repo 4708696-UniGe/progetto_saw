d336a2173acb7d1f6944587cd2fa2b5b6003e25f
4708696-UniGe <S4708696@studenti.unige.it>
2020-11-28 15:53:57 +0100

    style mod

--- php/footer.php
+++ php/footer.php
@@ -1,7 +1,7 @@
 <?php ?>
   <img id="logo" src="../images/logo.png" alt="Logo azienda">
 	<ul id"footer">
-	  <li id="email">Email: <a href="mailto:s4708696@studenti.unige.it">s4708696@studenti.unige.it</a></li>
+	  <li id="email">Email: <a title="Contattaci" href="mailto:s4708696@studenti.unige.it">s4708696@studenti.unige.it</a></li>
 	  <li id="phone">Telefono: +39 3479620471</li>
 	  <li id="nome_azienda">System Hospital S.r.l.</li>
 	  <li id="copyright">@ copyright 2020 - 2021</li>
