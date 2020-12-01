fc285698e1d1b75432e7509104a06e98bf3980da
4708696-UniGe <S4708696@studenti.unige.it>
2020-12-01 12:18:04 +0100

    finish style

--- php/footer.php
+++ php/footer.php
@@ -1,10 +1,28 @@
 <?php ?>
 <a title="Home" href="home.php">
   <img id="logo" src="../images/logo.png" alt="Logo azienda"></a>
+
+
+	<link rel="stylesheet" href="fontawesome/css/all.css">
+    <ul class="social_footer">
+      <li><a href="under_construction.php"><i class="fab fa-facebook" aria-hidden="true"></i></a></li>
+      <li><a href="under_construction.php"><i class="fab fa-instagram" aria-hidden="true"></i></a></li>
+      <li><a href="under_construction.php"><i class="fab fa-linkedin-in" aria-hidden="true"></i></a></li>
+      <li><a href="under_construction.php"><i class="fab fa-twitter" aria-hidden="true"></i></a></li>
+	  <li><a href="under_construction.php"><i class="fab fa-youtube" aria-hidden="true"></i></a></li>
+    </ul>
+
+
+
 	<ul id"footer">
 	  <li id="email">Email: <a title="Contattaci" href="mailto:s4708696@studenti.unige.it">s4708696@studenti.unige.it</a></li>
 	  <li id="phone">Telefono: +39 3479620471</li>
+	  <li id="piva">p. iva: 62396741670</li>
 	  <li id="nome_azienda">System Hospital S.r.l.</li>
 	  <li id="copyright">@ copyright 2020 - 2021</li>
 	</ul>
-	 <p id="piva">p. iva: 62396741670</p>
+
+
+
+
+	 
