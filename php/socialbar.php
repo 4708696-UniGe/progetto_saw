ab0586ccbcc6dfe78f673b13f9f0ab8305663e6c
Giulia Benvenuto <giulia.benvenuto@outlook.com>
2020-12-01 08:25:36 +0100

    socialbar

---
+++ php/socialbar.php
@@ -0,0 +1,26 @@
+
+
+<!DOCTYPE html>
+<html>
+	<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="Content-type" content="text/html; charset=UTF-8">
+
+	<title> Socialbar </title>
+	
+	<link rel="stylesheet" href="../css/socialbar.css">
+	<link rel="stylesheet" href="fontawesome/css/all.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+	</head>
+
+	<body>
+		<div class="icon-bar">
+			<a href="#" class="facebook"><i class="fab fa-facebook-f"></i></a>
+			<a href="#" class="twitter"><i class="fab fa-twitter"></i></a>
+			<a href="#" class="google"><i class="fab fa-google"></i></a>
+			<a href="#" class="linkedin"><i class="fab fa-linkedin-in"></i></a>
+			<a href="#" class="youtube"><i class="fab fa-youtube"></i></a>
+		</div>
+	</body>
+
+</html>
