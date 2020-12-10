9f960a8d987d07453e683434b68ab2693a68644b
4708696-UniGe <s4708696@studenti.unige.it>
2020-12-10 16:13:21 +0100

    responsive work on socialbar (absent on mobile)

--- php/socialbar.php
+++ php/socialbar.php
@@ -1,24 +1,13 @@
-<!DOCTYPE html>
-<html>
-	<head>
-	<meta charset="UTF-8">
-	<meta http-equiv="Content-type" content="text/html; charset=UTF-8">
+<?php ?>
 
-	<title> Socialbar </title>
-	
 	<link rel="stylesheet" href="../css/socialbar.css">
 	<link rel="stylesheet" href="fontawesome/css/all.css">
 	
-	</head>
 
-	<body>
-		<div class="icon-bar">
-			<a href="under_construction.php" class="facebook"><i class="fab fa-facebook-f"></i></a>
-			<a href="under_construction.php" class="twitter"><i class="fab fa-twitter"></i></a>
-			<a href="under_construction.php" class="google"><i class="fab fa-google"></i></a>
-			<a href="under_construction.php" class="linkedin"><i class="fab fa-linkedin-in"></i></a>
-			<a href="under_construction.php" class="youtube"><i class="fab fa-youtube"></i></a>
-		</div>
-	</body>
-
-</html>
+	<div class="icon-bar">
+		<a href="under_construction.php" class="facebook"><i class="fab fa-facebook-f"></i></a>
+		<a href="under_construction.php" class="twitter"><i class="fab fa-twitter"></i></a>
+		<a href="under_construction.php" class="google"><i class="fab fa-google"></i></a>
+		<a href="under_construction.php" class="linkedin"><i class="fab fa-linkedin-in"></i></a>
+		<a href="under_construction.php" class="youtube"><i class="fab fa-youtube"></i></a>
+	</div>
