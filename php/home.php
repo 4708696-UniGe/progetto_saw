9ec80bb5737e52487b649de17e271762eaa95aaf
4708696-UniGe <s4708696@studenti.unige.it>
2020-12-10 17:12:02 +0100

    add custom (test) scrollbar

--- php/home.php
+++ php/home.php
@@ -8,6 +8,7 @@
 	<link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 	<link rel="icon" href="../images/icon.png">
 	<link rel="stylesheet" href="../css/home.css">
+	<link rel="stylesheet" href="../css/scrollbar.css">
 	
 	
 	</head>
