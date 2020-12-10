845ab8b02aa166c51e2a4dcea2263abb9bc89b82
4708696-UniGe <s4708696@studenti.unige.it>
2020-12-10 10:45:41 +0100

    fix some side scrolling footer

--- php/platform_list.php
+++ php/platform_list.php
@@ -80,7 +80,7 @@
             </div>
 	
 	
-	
+	    <script src="../bootstrap/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 	
 		</body>
     	
