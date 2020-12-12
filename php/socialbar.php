6bb88b2ff161f5a50ca072b1786eb3997281cbc3
4708696-UniGe <s4708696@studenti.unige.it>
2020-12-12 22:21:59 +0100

    footer rework and socialbar adjustements

--- php/socialbar.php
+++ php/socialbar.php
@@ -5,9 +5,9 @@
 	
 
 	<div class="icon-bar">
-		<a href="under_construction.php" class="facebook"><i class="fab fa-facebook-f"></i></a>
+		<a href="under_construction.php" class="facebook"><i class="fab fa-facebook"></i></a>
 		<a href="under_construction.php" class="twitter"><i class="fab fa-twitter"></i></a>
-		<a href="under_construction.php" class="google"><i class="fab fa-google"></i></a>
+		<a href="under_construction.php" class="google"><i class="fab fa-instagram"></i></a>
 		<a href="under_construction.php" class="linkedin"><i class="fab fa-linkedin-in"></i></a>
 		<a href="under_construction.php" class="youtube"><i class="fab fa-youtube"></i></a>
 	</div>
