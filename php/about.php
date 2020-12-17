cc23ab4a95e650c47d7a2ef3e0618c1712b8654f
4708696-UniGe <s4708696@studenti.unige.it>
2020-12-17 12:23:03 +0100

    about work

--- php/about.php
+++ php/about.php
@@ -2,7 +2,7 @@
 
 <html lang="it">
 
-     <head>
+    <head>
 	<meta charset="UTF-8">
 	<meta http-equiv="Content-type" content="text/html; charset=UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
@@ -11,24 +11,21 @@
 	<link rel="icon" href="../images/icon.png">
 	<link rel="stylesheet" href="../css/about.css">
         <link rel="stylesheet" href="../css/scrollbar.css">
-     </head>	
+    </head>
 
 <body>
 
 		<nav> <?php include 'navbar.php'; ?></nav>
 		
 	<div class="container-about">
-		<div class="about">Siamo una startup nata da un progetto universitario del corso di Sviluppo Apllicazioni Web, con lo scopo iniziale di fornire assistenza alle piccole e medie imprese.
-Attualmente non disponiamo di una sede, anche a causa della corrente situazione ma potrete trovarci nei recapiti a fondo pagina (telefono ed email)</div>
+		<div class="about">Siamo una startup nata da un progetto universitario del corso di Sviluppo Applicazioni Web, con lo scopo iniziale di fornire assistenza alle piccole e medie imprese.
+            Attualmente non disponiamo di una sede, anche a causa della corrente situazione ma potrete trovarci nei recapiti a fondo pagina (telefono ed email)</div>
 
 		<div class="work-place"><img src="../images/workplace.jpeg" alt="Luogo di Lavoro"></div>
 
 	</div>
 
 	<footer> <?php include 'footer.php'; ?> </footer>
-
-
-
 
 	<script src="../bootstrap/js/bootstrap.bundle.min.js"  crossorigin="anonymous"></script>
 	</body>
