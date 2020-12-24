68583443ff45a48bfb89b9f23efcd8a96847ef56
4708696-UniGe <s4708696@studenti.unige.it>
2020-12-24 16:43:47 +0100

    fix

--- php/footer.php
+++ php/footer.php
@@ -1,52 +1,54 @@
- <link rel="stylesheet" href="../css/footer.css">
-            <div class="container">
-                <div class="row ">
-                    
-                    <div class="col-md-4  text-white my-4 text-center text-md-left ">
-                        <span class=" font-weight-bold ">I nostri canali social</span>
-                        <div class="py-5">
-                            <a href="under_construction.php"><i class="fab fa-facebook fa-2x text-primary mx-3"></i></a>
-                            <a href="under_construction.php"><i class="fab fa-instagram fa-2x text-danger mx-3"></i></a>
-                            <a href="under_construction.php"><i class="fab fa-linkedin-in fa-2x text-info mx-3"></i></a>
-                            <a href="under_construction.php"><i class="fab fa-twitter fa-2x text- mx-3"></i></a>
-                            <a href="under_construction.php"><i class="fab fa-youtube fa-2x text-danger mx-3"></i></a>
-                        </div>
-                    </div>                    
-                    
-                    <div class="col-md-4 text-center text-md-left ">
-                        
-                        <div class="py-3">
-                            <img id="logo" src="../images/logo.png" alt="Logo azienda"></a>
-    
-                            <p class="footer-links font-weight-bold">
-                                <a class="text-white" href="home.php">Home</a>
-                                |
-                                <a class="text-white" href="#">Blog</a>
-                                |
-                                <a class="text-white" href="about.php">Chi siamo</a>
-                                |
-                                <a class="text-white" href="#">Contattaci</a>
-                            </p>
-                            <p class="text-light py-3 mb-4">&copy;2020 System Hospital S.r.l.</p>
-                        </div>
-                    </div>
-                    
-                    <div class="col-md-4 text-white text-right text-md-left ">
-                        <div class="py-2 my-4">
-                            <div>
-                                <p class="text-white"> <i><img src="../images/geo.svg" alt="" width="30" height="30" title="Posizione"></i>
-                                        Via All&#39;opera Pia 15 - Genova - 16145 - GE</p>
-                            </div>
-    
-                            <div> 
-                                <p><i><img src="../images/phone.svg" alt="" width="30" height="30" title="Telefono"></i> +39 3479620471</p>
-                            </div>
-                            <div>
-                                <p><i><img src="../images/mailbox2.svg" alt="" width="30" height="30" title="E-mail"></i><a href="mailto:s4708696@studenti.unige.it">s4708696@studenti.unige.it</a></p>
-                            </div>  
-                        </div>  
-                    </div>
-                    
+<link rel="stylesheet" href="../css/footer.css">
+<link rel="stylesheet" href="fontawesome/css/all.css">
 
-                </div>  
+<div class="container">
+    <div class="row ">
+
+        <div class="col-md-4  text-white my-4 text-center text-md-left ">
+            <span class=" font-weight-bold ">I nostri canali social</span>
+            <div class="py-5">
+                <a href="under_construction.php"><i class="fab fa-facebook fa-2x text-primary mx-3"></i></a>
+                <a href="under_construction.php"><i class="fab fa-instagram fa-2x text-danger mx-3"></i></a>
+                <a href="under_construction.php"><i class="fab fa-linkedin-in fa-2x text-info mx-3"></i></a>
+                <a href="under_construction.php"><i class="fab fa-twitter fa-2x text- mx-3"></i></a>
+                <a href="under_construction.php"><i class="fab fa-youtube fa-2x text-danger mx-3"></i></a>
             </div>
+        </div>
+
+        <div class="col-md-4 text-center text-md-left ">
+
+            <div class="py-3">
+                <img id="logo" src="../images/logo.png" alt="Logo azienda"></a>
+
+                <p class="footer-links font-weight-bold">
+                    <a class="text-white" href="home.php">Home</a>
+                    |
+                    <a class="text-white" href="#">Blog</a>
+                    |
+                    <a class="text-white" href="about.php">Chi siamo</a>
+                    |
+                    <a class="text-white" href="#">Contattaci</a>
+                </p>
+                <p class="text-light py-3 mb-4">&copy;2020 System Hospital S.r.l.</p>
+            </div>
+        </div>
+
+        <div class="col-md-4 text-white text-right text-md-left ">
+            <div class="py-2 my-4">
+                <div>
+                    <p class="text-white"> <i><img src="../images/geo.png" alt="" width="30" height="30" title="Posizione"></i>
+                        Via All&#39;opera Pia 15 - Genova - 16145 - GE</p>
+                </div>
+
+                <div>
+                    <p><i><img src="../images/phone.png" alt="" width="30" height="30" title="Telefono"></i> +39 3479620471</p>
+                </div>
+                <div>
+                    <p><i><img src="../images/mailbox2.png" alt="" width="30" height="30" title="E-mail" ></i><a id="email-icon" href="mailto:s4708696@studenti.unige.it"> s4708696@studenti.unige.it</a></p>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+</div>
