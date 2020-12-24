57bb6b621072829e138e5ef908f8c1b36a12e1e7
4708696-UniGe <s4708696@studenti.unige.it>
2020-12-24 16:48:05 +0100

    fix

--- php/products.php
+++ php/products.php
@@ -201,7 +201,7 @@
                 </div>
                 </div>
               </div>
-          </div>
+          </>
           
           <div class="price-title-mobile">
           <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
