83ffd5c9b8f8f0e086b3f4a432d0c820ea19af8a
Giulia Benvenuto <giulia.benvenuto@outlook.com>
2020-12-26 11:17:17 +0100

    modifica bottoni

--- php/profile.php
+++ php/profile.php
@@ -27,13 +27,14 @@
                   <div class="card">
                     <div class="card-body">
                       <div class="d-flex flex-column align-items-center text-center">
-                        <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="Admin" class="rounded-circle" width="150">
+                   
                         <div class="mt-3">
                           <h4> <?php echo $_COOKIE['FIRSTNAME']." ".$_COOKIE['LASTNAME']; ?> </h4>
                           <p class="text-secondary mb-1">Full Stack Developer</p>
                           <p class="text-muted font-size-sm">Bay Area, San Francisco, CA</p>
-                          <button class="btn btn-primary">Follow</button>
-                          <button class="btn btn-outline-primary">Message</button>
+                          <form action="profile_mod.php">
+                          <button class="btn btn-primary">Modifica profilo</button>
+                          </form>
                         </div>
                       </div>
                     </div>
