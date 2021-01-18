2fb5650d858c06d9411ca255c612203cd780945d
Riccardo Guerrisi <S4639439@studenti.unige.it>
2021-01-18 17:16:01 +0100

    cart inizio

--- php/navbar.php
+++ php/navbar.php
@@ -1,14 +1,4 @@
-<?php
-/*<?php  ?>
-<a title="Home" href="home.php"><img id="logotop" src="../images/logo.png" alt="Logo azienda"> </a>
- <ul id="nav">
-  <li id="about"><a href="about.php">About</a> </li>
-  <li id="signup"><a href="registration.php">Sign-Up</a> </li>
-  <li id="login"><a href="login.php">Login</a> </li>
-  </a> </li>
- </ul> */ ?>
- 
-<link rel="stylesheet" href="../css/navbar.css">
+ <link rel="stylesheet" href="../css/navbar.css">
 
  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
    <div class="container-fluid">
