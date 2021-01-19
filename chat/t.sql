05e55b2cdf71c2411b52b47fd7b17769627dd872
Giulia Benvenuto <giulia.benvenuto@outlook.com>
2021-01-19 08:56:37 +0100

    sql

--- chat/t.sql
+++ chat/t.sql
@@ -1,12 +1,6 @@
---
--- Database: `chat`
---
 
--- --------------------------------------------------------
 
---
--- Table structure for table `chat_message`
---
+-- Struttura tabella chat_message
 
 CREATE TABLE `chat_message` (
   `chat_message_id` int(11) NOT NULL,
@@ -17,43 +11,17 @@ CREATE TABLE `chat_message` (
   `status` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
 
---
--- Table structure for table `login`
---
 
-CREATE TABLE `login` (
-  `user_id` int(11) NOT NULL,
-  `username` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `login`
---
-
-INSERT INTO `login` (`user_id`, `username`, `password`) VALUES
-(1, 'johnsmith', '$2y$10$4REfvTZpxLgkAR/lKG9QiOkSdahOYIR3MeoGJAyiWmRkEFfjH3396'),
-(2, 'peterParker', '$2y$10$4REfvTZpxLgkAR/lKG9QiOkSdahOYIR3MeoGJAyiWmRkEFfjH3396'),
-(3, 'davidMoore', '$2y$10$4REfvTZpxLgkAR/lKG9QiOkSdahOYIR3MeoGJAyiWmRkEFfjH3396');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `login_details`
---
+-- Struttura tabella login_details
 
 CREATE TABLE `login_details` (
   `login_details_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
-  `last_activity` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `is_type` enum('no','yes') NOT NULL
+  `last_activity` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Indexes for dumped tables
---
 
 --
 -- Indexes for table `chat_message`
@@ -61,11 +29,6 @@ CREATE TABLE `login_details` (
 ALTER TABLE `chat_message`
   ADD PRIMARY KEY (`chat_message_id`);
 
---
--- Indexes for table `login`
---
-ALTER TABLE `login`
-  ADD PRIMARY KEY (`user_id`);
 
 --
 -- Indexes for table `login_details`
@@ -73,9 +36,6 @@ ALTER TABLE `login`
 ALTER TABLE `login_details`
   ADD PRIMARY KEY (`login_details_id`);
 
---
--- AUTO_INCREMENT for dumped tables
---
 
 --
 -- AUTO_INCREMENT for table `chat_message`
@@ -83,11 +43,6 @@ ALTER TABLE `login_details`
 ALTER TABLE `chat_message`
   MODIFY `chat_message_id` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT for table `login`
---
-ALTER TABLE `login`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `login_details`
