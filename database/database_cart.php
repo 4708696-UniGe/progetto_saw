3059077c7234024b520ef74b90d6576c8cd495a8
Riccardo Guerrisi <S4639439@studenti.unige.it>
2021-01-20 00:06:53 +0100

    cart

--- database/database_cart.php
+++ database/database_cart.php
@@ -7,5 +7,45 @@ $con = mysqli_connect("localhost","root","","test");
  die();
  }
 
+ if(!empty($_GET["action"])) {
+	switch($_GET["action"]) {
+		case "add":
+				$result = mysqli_query($con, "SELECT * FROM products WHERE code='" . $_GET["code"] . "'");
+				while($row = mysqli_fetch_assoc($result)){
+					$productByCode[] = $row;
+				}
+				$itemArray = array($productByCode[0]["code"]=>array('name'=>$productByCode[0]["name"], 'code'=>$productByCode[0]["code"],  'price'=>$productByCode[0]["price"]));
+		
+				if(!empty($_SESSION["cart_item"])) {
+					if(in_array($productByCode[0]["code"],array_keys($_SESSION["cart_item"]))) {
+						foreach($_SESSION["cart_item"] as $k => $v) {
+								if($productByCode[0]["code"] == $k) {
+									echo "<script>var flag_item= 1</script>";
+								}
+						}
+					} else {
+						$_SESSION["cart_item"] = array_merge($_SESSION["cart_item"],$itemArray);
+						$query = "INSERT TO cart "
+					}
+				} else {
+					$_SESSION["cart_item"] = $itemArray;
+				}
+			break;
 
+		case "remove":
+		if(!empty($_SESSION["cart_item"])) {
+			foreach($_SESSION["cart_item"] as $k => $v) {
+				if($_GET["code"] == $k)
+					unset($_SESSION["cart_item"][$k]);				
+				if(empty($_SESSION["cart_item"]))
+					unset($_SESSION["cart_item"]);
+			}	
+		}
+		break;
+
+		case "empty":
+			unset($_SESSION["cart_item"]);
+			break;
+	}
+}
 ?>
