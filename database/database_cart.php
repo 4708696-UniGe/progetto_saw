c5876a46273a6e4462bf6c7491647ac5456e92f5
Riccardo Guerrisi <S4639439@studenti.unige.it>
2021-01-22 18:34:46 +0100

    fix merge cart

--- database/database_cart.php
+++ database/database_cart.php
@@ -2,90 +2,113 @@
 
 
 $con = mysqli_connect("localhost","root","","test");
-    if (mysqli_connect_errno()){
- echo "Failed to connect to MySQL: " . mysqli_connect_error();
- die();
- }
+if (mysqli_connect_errno()){
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    die();
+}
 
- if(!empty($_GET["action"])) {
-	switch($_GET["action"]) {
-		case "add":
-				$result = mysqli_query($con, "SELECT * FROM products WHERE code='" . $_GET["code"] . "'");
-				while($row = mysqli_fetch_assoc($result)){
-					$productByCode[] = $row;
-				}
-				$itemArray = array($productByCode[0]["code"]=>array('name'=>$productByCode[0]["name"], 'code'=>$productByCode[0]["code"],  'price'=>$productByCode[0]["price"]));
-		
-				if(!empty($_SESSION["cart_item"])) {
-					if(in_array($productByCode[0]["code"],array_keys($_SESSION["cart_item"]))) {
-						foreach($_SESSION["cart_item"] as $k => $v) {
-								if($productByCode[0]["code"] == $k) {
-									echo "<script>var flag_item= 1</script>";
-								}
-						}
-					} else {
-						$_SESSION["cart_item"] = array_merge($_SESSION["cart_item"],$itemArray);
-						$query = "INSERT INTO cart (email, product_code) VALUES ('{$_SESSION["EMAIL"]}','{$_GET["code"]}');";
-						mysqli_query($con, $query);
-						if (mysqli_affected_rows($con) != 1) {
-							echo "Attenzione c'è stato un problema nell'inserimento, controlla i dati. ".mysqli_error($con);
-						}
-					}
-				} else {
-					$_SESSION["cart_item"] = $itemArray;
-					$query = "INSERT INTO cart (email, product_code) VALUES ('{$_SESSION["EMAIL"]}','{$_GET["code"]}');";
-					mysqli_query($con, $query);
-					if (mysqli_affected_rows($con) != 1) {
-						echo "Attenzione c'è stato un problema nell'inserimento, controlla i dati. ".mysqli_error($con);
-					}
-				}
-			break;
 
-		case "remove":
-		if(!empty($_SESSION["cart_item"])) {
-			foreach($_SESSION["cart_item"] as $k => $v) {
-				if($_GET["code"] == $k){
-					$query = "DELETE FROM cart WHERE email = '{$_SESSION["EMAIL"]}' AND WHERE product_code = '{$_SESSION["cart_item"][$k]}'";
-					mysqli_query($con, $query);
-					if (mysqli_affected_rows($con) != 1) {
-						echo "Attenzione c'è stato un problema nell'inserimento, controlla i dati. ".mysqli_error($con);
-					}
-					unset($_SESSION["cart_item"][$k]);
-				}
-				if(empty($_SESSION["cart_item"]))
-					unset($_SESSION["cart_item"]);
-			}	
-		}
-		break;
+$query_cart = "CREATE TABLE IF NOT EXISTS cart (
+   email VARCHAR(50),
+   product_code VARCHAR(10) PRIMARY KEY,
+)";
+$query_products = "CREATE TABLE products (
+  id int(8) NOT NULL AUTO INCREMENT ,
+  name varchar(30) NOT NULL,
+  code varchar(10) NOT NULL,
+  price double(5,2) NOT NULL
+)";
 
-		case "empty":
-			$query = "DELETE FROM cart WHERE email = '{$_SESSION["EMAIL"]}'";
-			mysqli_query($con, $query);
-			if (mysqli_affected_rows($con) == 0) {
-				echo "Attenzione c'è stato un problema nell'inserimento, controlla i dati. ".mysqli_error($con);
-			}
-			unset($_SESSION["cart_item"]);
-			break;
-	}
+
+
+
+if(!empty($_GET["action"])) {
+    switch($_GET["action"]) {
+        case "add":
+
+            $result = mysqli_query($con, "SELECT * FROM products WHERE code='" . $_GET["code"] . "'");
+            while($row = mysqli_fetch_assoc($result)){
+                $productByCode[] = $row;
+            }
+            $itemArray = array($productByCode[0]["code"]=>array('name'=>$productByCode[0]["name"], 'code'=>$productByCode[0]["code"],  'price'=>$productByCode[0]["price"]));
+
+
+
+            if(!empty($_SESSION["cart_item"])) {
+                if(in_array($productByCode[0]["code"],array_keys($_SESSION["cart_item"]))) {
+                    foreach($_SESSION["cart_item"] as $k => $v) {
+                        if($productByCode[0]["code"] == $k) {
+                            echo "<script>var flag_item= 1</script>";
+                            break;
+                        }
+                    }
+                }else{
+                    echo "<script>var flag_item = 2</script>";
+                }
+            } else {
+                $_SESSION["cart_item"] = $itemArray;
+                $query = "INSERT INTO cart (email, product_code) VALUES ('{$_SESSION["EMAIL"]}','{$_GET["code"]}');";
+                mysqli_query($con, $query);
+                if (mysqli_affected_rows($con) != 1) {
+                    echo "Attenzione c'ï¿½ stato un problema nell'inserimento, controlla i dati. ".mysqli_error($con);
+                }
+            }
+            break;
+
+        case "remove":
+            if(!empty($_SESSION["cart_item"])) {
+                foreach($_SESSION["cart_item"] as $k => $v) {
+                    if($_GET["code"] == $k){
+                        $query = "DELETE FROM cart WHERE email = '{$_SESSION["EMAIL"]}' AND product_code = '{$_SESSION["cart_item"][$k]["code"]}'";
+                        mysqli_query($con, $query);
+                        if (mysqli_affected_rows($con) != 1) {
+                            echo "Attenzione c'ï¿½ stato un problema nell'inserimento, controlla i dati. ".mysqli_error($con);
+                        }
+                        unset($_SESSION["cart_item"][$k]);
+                    }
+                    if(empty($_SESSION["cart_item"]))
+                        unset($_SESSION["cart_item"]);
+                }
+            }
+            break;
+
+        case "empty":
+            if(!empty($_SESSION["cart_item"])) {
+                $query = "DELETE FROM cart WHERE email = '{$_SESSION["EMAIL"]}'";
+                mysqli_query($con, $query);
+                if (mysqli_affected_rows($con) == 0) {
+                    echo "Attenzione c'ï¿½ stato un problema nell'inserimento, controlla i dati. " . mysqli_error($con);
+                }
+                unset($_SESSION["cart_item"]);
+                break;
+            }else{}
+    }
 }
 
 $query = "SELECT product_code FROM cart WHERE email = '{$_SESSION["EMAIL"]}'";
 $result = mysqli_query($con, $query);
 $array = mysqli_fetch_all($result);
 if (mysqli_affected_rows($con) != 0) {
-	foreach($array as $value){
-		$qry = "SELECT * FROM products WHERE code = '".$value[0]."'";
-		$result2 = mysqli_query($con, $qry);
-		if(mysqli_affected_rows($con) != 1){
-			echo "problema";
-		}else{
-			while($row = mysqli_fetch_assoc($result2)){
-				$productByCode[] = $row;
-			}
-			$itemArray = array($productByCode[0]["code"]=>array('name'=>$productByCode[0]["name"], 'code'=>$productByCode[0]["code"],  'price'=>$productByCode[0]["price"]));
-			$_SESSION["cart_item"] = $itemArray;
-		}
-	}
-	
+    $i=0;
+    foreach($array as $value){
+        $qry = "SELECT * FROM products WHERE code = '".$value[0]."'";
+        $result2 = mysqli_query($con, $qry);
+        if(mysqli_affected_rows($con) != 1){
+            echo "problema";
+        }else{
+            while($row = mysqli_fetch_assoc($result2)){
+                $product[] = $row;
+            }
+            $itemArray2 = array($product[$i]["code"]=>array('name'=>$product[$i]["name"], 'code'=>$product[$i]["code"],  'price'=>$product[$i]["price"]));
+            if(empty($_SESSION["cart_item"])) {
+                $_SESSION["cart_item"] = $itemArray2;
+            }
+            else if (!empty($_SESSION["cart_item"])) {
+                $_SESSION["cart_item"] = array_merge($_SESSION["cart_item"], $itemArray2);
+            }
+        }
+        $i++;
+    }
 }
+
 ?>
--- php/cart.php
+++ php/cart.php
@@ -5,87 +5,98 @@ include ("../database/database_cart.php");
 ?>
 <!DOCTYPE html>
 <html lang="it">
-	<head>
-		<meta charset="UTF-8">
-		<meta http-equiv="Content-type" content="text/html; charset=UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title> System Hospital - Carrello </title>
-		<link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
-		<link rel="stylesheet" href="../css/cart.css">
-		<link rel="icon" href="../images/icon.png">
-	</head>
-	
-	<nav> <?php include 'navbar.php'; ?></nav>
-	
-	<div id="shopping-cart">
-		<p id="flag_cart" class="alert alert-danger devisible" role="alert"></p>
-		<a id="btnEmpty" href="cart.php?action=empty">Svuota il carrello</a>
-	<?php
-		if(isset($_SESSION["cart_item"])){
-			$total_price = 0;
-	?>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="Content-type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title> System Hospital - Carrello </title>
+    <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/cart.css">
+    <link rel="icon" href="../images/icon.png">
+</head>
 
-	
-	<body>
-	<table class="table">
-	<thead>
-		<tr>
-			<th scope = "col">Nome</th>
-			<th scope = "col">Codice</th>
-			<th scope = "col">Prezzo</th>
-			<th scope = "col" style="text-align:center;">Rimuovi</th>
-		</tr>
-	</thead>
-	<tbody>
-		<?php		
-			foreach ($_SESSION["cart_item"] as $item){
-				$item_price =$item["price"];
-				?>
-						<tr>
-							<td><?php echo $item["name"]; ?></td>
-							<td><?php echo $item["code"]; ?></td>
-							<?php if($item["code"] == "bronze2" || $item["code"] == "silver2" || $item["code"] == "gold2"){
-								echo "<td>&euro;".number_format($item_price,2)." / MESE</td>";
-							} else if ($item["code"] == "bronze1" || $item["code"] == "silver1" || $item["code"] == "gold1"){
-								echo "<td>&euro;".number_format($item_price,2)."</td>";
-							}
-							?>
-							<td style="text-align:center;"><a href="cart.php?action=remove&code=<?php echo $item["code"]; ?>" class="btnRemoveAction"><img src="../images/icon-delete.png" alt="Rimuovi Oggetto" /></a></td>
-						</tr>
-						<?php
-						$total_price += ($item["price"]);
-				}
-				?>
+<nav> <?php include 'navbar.php'; ?></nav>
 
-		<tr>
-			<td></td>
-			<td></td>
-			<td></td>
-			<td style="text-align:center;">Total: <strong><?php echo "&euro; ".number_format($total_price, 2); ?></strong></td>
-		</tr>
-	</tbody>
-	</table>		
-	  <?php
-	} else {
-	?>
-	<div class="empty_cart">
-		<div class="no-records">Il carrello &egrave; vuoto.</div>
-		<a id="btnbuy" class="w-80 btn btn-lg btn-primary" href="products.php" role="button">Acquista prodotti</a>
-	</div>
-	<?php 
-	}
-	?>
-	</div>
-	</div>
-	<script src="../bootstrap/js/bootstrap.bundle.min.js"  crossorigin="anonymous"></script>
-	<script> 
-			window.onload = flag_cart();
-			function flag_cart() {
-				if(flag_item == 1) {
-				document.getElementById("flag_cart").className = "alert alert-danger box";
-				document.getElementById("flag_cart").innerHTML = "Prodotto gi&agrave; presente nel carrello.";
-				}
-			}
-	</script>
-	</body>
+<div id="shopping-cart">
+    <div class = "cart_alert">
+        <p id="flag_cart" class="alert alert-danger devisible" role="alert"></p>
+    </div>
+
+    <?php
+    if(isset($_SESSION["cart_item"])){
+    echo "<a class='btn btn-dark buymore' href='products.php'>Compra altro</a>";
+    $total_price = 0;
+    ?>
+
+
+    <body>
+    <table class="table">
+        <thead>
+        <tr>
+            <th scope = "col">Nome</th>
+            <th scope = "col">Codice</th>
+            <th scope = "col">Prezzo</th>
+            <th scope = "col" style="text-align:center;">Rimuovi</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php
+        foreach ($_SESSION["cart_item"] as $item){
+            $item_price =$item["price"];
+            ?>
+            <tr>
+                <td><?php echo $item["name"]; ?></td>
+                <td><?php echo $item["code"]; ?></td>
+                <?php if($item["code"] == "bronze2" || $item["code"] == "silver2" || $item["code"] == "gold2"){
+                    echo "<td>&euro;".number_format($item_price,2)." / MESE</td>";
+                } else if ($item["code"] == "bronze1" || $item["code"] == "silver1" || $item["code"] == "gold1"){
+                    echo "<td>&euro;".number_format($item_price,2)."</td>";
+                }
+                ?>
+                <td style="text-align:center;"><a href="cart.php?action=remove&code=<?php echo $item["code"]; ?>" class="btnRemoveAction"><img src="../images/icon-delete.png" alt="Rimuovi Oggetto" /></a></td>
+            </tr>
+            <?php
+            $total_price += ($item["price"]);
+        }
+        ?>
+
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td style="text-align:center;">Total: <strong><?php echo "&euro; ".number_format($total_price, 2); ?></strong></td>
+        </tr>
+        </tbody>
+    </table>
+    <div class="cart_button">
+        <a class="btn btn-danger" href='cart.php?action=empty'>Svuota il carrello</a>
+        <a class="btn btn-success" href='../database/database_checkout.php'>Procedi all'acquisto</a>
+    </div>
+    <?php
+    } else {
+        ?>
+        <div class="empty_cart">
+            <div class="no-records">Il carrello &egrave; vuoto.</div>
+            <a id="btnbuy" class="w-80 btn btn-lg btn-primary" href="products.php" role="button">Acquista prodotti</a>
+        </div>
+        <?php
+    }
+    ?>
+</div>
+</div>
+<script src="../bootstrap/js/bootstrap.bundle.min.js"  crossorigin="anonymous"></script>
+<script>
+    window.onload = flag_cart();
+    function flag_cart() {
+        if(flag_item == 1) {
+            document.getElementById("flag_cart").className = "alert alert-danger box";
+            document.getElementById("flag_cart").innerHTML = "Prodotto gi&agrave; presente nel carrello.";
+        }
+        else if(flag_item == 2) {
+            document.getElementById("flag_cart").className = "alert alert-danger box";
+            document.getElementById("flag_cart").innerHTML = "Puoi acquistare un solo abbonamento/pacchetto.";
+        }
+    }
+</script>
+</body>
 </html>
